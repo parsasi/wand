d1c403332d161b050f36f5a1695d164037f15177
parsasi <mail@parsa.pro>
2024-03-06 11:45:13 -0800

    Added `fo` binding to trigger formatting

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,3 +1,4 @@
+
 -- Read the docs: https://www.lunarvim.org/docs/configuration
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
@@ -17,7 +18,7 @@ formatters.setup {
     ---@usage arguments to pass to the formatter
     -- these cannot contain whitespace
     -- options such as `--line-width 80` become either `{"--line-width", "80"}` or `{"--line-width=80"}`
-    args = { "--print-width", "100" },
+    -- args = { "--print-width", "100" },
     ---@usage only start in these filetypes, by default it will attach to all filetypes it supports
     filetypes = {
       "css",
@@ -59,6 +60,7 @@ vim.api.nvim_set_keymap('n', '<Space>sg', ':Telescope live_grep<CR>', { noremap 
 vim.api.nvim_set_keymap('n', '<Space>aa', 'ggVG', { noremap = true, silent = true })
 lvim.keys.normal_mode["gt"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["GT"] = ":BufferLineCyclePrev<CR>"
+vim.keymap.set('n', 'fo', function() vim.lsp.buf.format() end, { noremap = true })
 
 -- Change Vimtree width
 lvim.builtin.nvimtree.setup.view.width = 55

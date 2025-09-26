bd9daace9e0983b14df7d89eaf11338326697cfb
parsasi <mail@parsa.pro>
2025-09-25 22:57:01 -0700

    Removed extra files

--- setup.sh
+++ setup.sh
@@ -26,16 +26,6 @@ ln -s ~/wand/.zshrc ~/.zshrc
 # Create a symlink for ~/.config/lvim to ~/wand/lvim
 ln -s ~/wand/lvim ~/.config/lvim
 
-# Prompt for the prod context name
-read -p "Enter the context name for bnw prod: " prod_context
-echo "$prod_context" > "$(dirname "$0")/bnw-prod.txt"
-
-read -p "Enter the context name for bnw staging: " staging_context
-echo "$staging_context" > "$(dirname "$0")/bnw-staging.txt"
-
-read -p "Enter your ChatGPT API Key: " chatgpt_api_key
-echo "$chatgpt_api_key" > "$(dirname "$0")/.chatgpt-api-key"
-
 
 # Check if target directory is provided as argument
 if [ $# -eq 0 ]; then
